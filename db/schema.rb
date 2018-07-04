e34822a729adfc7514ff065a5c267ad6a03ad014
Ezekiel Mugaya <emugaya@gmail.com>
2018-07-04 18:21:56 +0300

    feat(user-basic-auth): Enable users to signin, signout, and signout
    - Enable users to signin
    - Enable users to signup
    - Enables users to reset password
    - Enables users to signout
    
    [Delivers 158519654]

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 2018_06_27_185553) do
     t.string 'first_name'
     t.string 'last_name'
     t.string 'email_address'
-    t.string 'password'
+    t.string 'password_digest'
+    t.string 'answer'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
